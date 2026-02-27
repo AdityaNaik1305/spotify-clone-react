7500046cf9544fe117853de0a3cd792baedfebd8
Aditya Naik <adityanaik1305@gmail.com>
2026-02-28 00:03:14 +0530

    Updated README

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/spotify-clone-react/">
     <PlayerContextProvider>
       <App />
     </PlayerContextProvider>
